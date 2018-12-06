f7bebce8efb80eb3771753fae3e032785cc6657c
Erwin <k3s47@outlook.com>
2018-12-06 22:29:27 +0700

    Remove import
    Remove unused library

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	$(function () {
-		$("#datepicker").datepicker();
-		console.log('asdasd')
-	});
-	
   res.render('index', { title: 'FullStack Vietnam' });
 });
 
